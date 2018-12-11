f7196219b2d8264ff6b2a28eba769de3e3ebfd1f
leonel223 <leonel_chitrit@hotmail.com>
2018-12-11 11:30:03 -0300

    temas y js

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -8,6 +8,7 @@
       <link rel="stylesheet" href="proyecto/Proyecto/TpFinal/eliantoFont/stylesheet.css" type="text/css" charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <link rel="stylesheet" href="proyecto/Proyecto/TpFinal/css/formStyles.css" id="theme2_css">
+      <link rel="stylesheet" href="proyecto/Proyecto/TpFinal/css/pisobootstrap.css">
     <meta charset="utf-8">
     <title></title>
   </head>
